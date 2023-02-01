b6a8d3d2d03b66a7fd2e617bb1dc560f6a1c054d
chandru0903 <122546215+chandru0903@users.noreply.github.com>
2023-02-01 22:48:55 +0530

    Add files via upload

---
+++ replace the alphabet.py
@@ -0,0 +1,8 @@
+str1 = input("please enter your own string :")
+ch = input("please enter your own character :")
+newh = input("please enter the new character :")
+
+str2 = str1.replace(ch,newch)
+
+print("\nOriginal string : " ,str1)
+print("Modified string : " , str2)
